71bce36a3bb9333bc60d64d901d5246b51cc23c2
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-31 19:08:24 +0300

    add homework task 013

---
+++ HW_task013/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+else
+{
+    // string textNumber = Convert.ToString(num);
+    // string signThird = Convert.ToString(textNumber[2]);
+    string signThird = Convert.ToString(Convert.ToString(num)[2]);
+    Console.WriteLine($"{num} -> третья цифра {signThird}");
+}
