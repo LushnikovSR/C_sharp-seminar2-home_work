7e83b1516f9f6d17c7cc1f7764ff041a49cb983f
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-31 19:07:36 +0300

    add homework task 010

---
+++ HW_task010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трёхзначное целое число: ");
+string num = Console.ReadLine();
+if (num.Length == 3)
+{
+    string signSecond = Convert.ToString(num[1]);
+    Console.WriteLine($"{num} -> {signSecond}");
+}
+else
+{
+    Console.WriteLine($"Введено неверное число");
+    Console.WriteLine("Введите трёхзначное целое число");
+    Console.WriteLine("Например: 748");
+}
